01674edf626cd9dfed8caa3271ea591601f9a6d9
dasbi <dasbi@DESKTOP-G89LPAG>
2023-02-09 13:03:52 +0530

    1st Test Commit

---
+++ src/Sahoo.java
@@ -0,0 +1,17 @@
+
+public class Sahoo {
+
+	public static void main(String[] args) {
+		int x=10;
+		int y=20;
+		int t = 0 ;
+		System.out.println("Before swap:"+x+" "+y);
+		t=x;
+		x=y;
+		y=t;
+		System.out.println("After swap:"+x+" "+y);
+		
+
+	}
+
+}
